1f28b85372a4dfe0431d6ae540af6c1f416d92b9
Jack Renoson <jayjay.rokers@gmail.com>
2025-12-23 01:59:27 +0100

    Cleanup Comments

--- src/main/java/spireQuests/quests/MarkNodeQuest.java
+++ src/main/java/spireQuests/quests/MarkNodeQuest.java
@@ -15,18 +15,19 @@ public interface MarkNodeQuest {
 
     /**
      * This is called to mark nodes when the quest is picked up.
+     * You need to implement this method; it will be called by the logic in MarkNodesOnMapPatch.
+     * Make sure to use rng for any random selection (not rng()!) and make sure rng is used the same on pickup, and on save and reload.
+     * Use ShowMarkedNodesOnMapPatch.MarkNode() in the function to mark nodes.
      * @param map The map of slay the spire, as a list of rows, which are lists of nodes.
      * @param rng the variables used to randomly determine stuff
-     */// You should likely need to write this, but not use it yourself.
-       // Make sure to use rng for any random selection (not rng()!) and make sure rng is used the same on pickup, and on save and reload.
-       // Use ShowMarkedNodesOnMapPatch.MarkNode() in the function to mark nodes.
+     */
     void markNodes(ArrayList<ArrayList<MapRoomNode>> map, Random rng);
 
     /**
      * Automatically generate a new Random based on seed, act number, and quest id.
+     * You only need to @Override this if the marks added by your quest depend on factors other than the current act or seed (like TreasureMapQuest, which depends on the node the quest was picked up at).
      * @return new Random for determining stuff randomly
-     */// You should likely not need to use this.
-       // Override this if your quest has more variables that you want to have influence rng (like TreasureMapQuest)
+     */
     default Random rng() {
         return new Random(Settings.seed ^ AbstractDungeon.actNum * 31L ^ ((AbstractQuest) this).id.hashCode());
     }
